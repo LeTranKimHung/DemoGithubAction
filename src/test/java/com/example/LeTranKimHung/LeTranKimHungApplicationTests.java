7500e4fda28ca378a5165c43c27d4618ae399738
LeTranKimHung <hungltk2004@gmail.com>
2026-03-19 10:22:01 +0700

    Initial commit - Spring Boot Docker

---
+++ src/test/java/com/example/LeTranKimHung/LeTranKimHungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeTranKimHung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeTranKimHungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
